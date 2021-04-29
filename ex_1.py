880ace74a32132a3c9f83f1145d210abba1ef60a
Oscar <ogcg12@gmail.com>
2021-04-28 23:30:33 -0500

    Added some beyong the exercises part

--- ex_1.py
+++ ex_1.py
@@ -2,7 +2,8 @@ import random
 
 def guessing_game():
     rand_num = random.randint(0, 100)
-    while True:
+    won = False
+    for _ in range(3):
         guess = int(input("Guess the number between 0-100: "))
         if guess > rand_num:
             print("Too high")
@@ -10,6 +11,10 @@ def guessing_game():
             print("Too low")
         else:
             print("Congrats you did it!")
+            won = True
             break
+        
+    if not won:
+        print(f"The number was {rand_num}, Game Over")
 
 guessing_game()
